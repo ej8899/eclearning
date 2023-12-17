39d133e3153e280da88cceaee455d54a35303675
ej8899 <ej8899@gmail.com>
2023-12-17 09:02:57 -0500

    add: applied python cryptography course work folder

---
+++ applied-python-cryptography/refresher.py
@@ -0,0 +1,17 @@
+import random
+import os
+
+from math import sqrt
+
+my_name = 'Bob'
+my_age = 53
+my_height = 1.83
+
+my_list = [1, "text", 4.5, 5]
+
+my_dictionary = {'name': 'bob', 'age': 53, 'height': 1.83}
+
+print (my_dictionary['name'])
+
+print (my_name[0].isupper())
+print (my_name[0].islower())
