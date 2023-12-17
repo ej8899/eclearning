59bb3d7a364a868dcce50cfb5300563ab089e9d3
ej8899 <ej8899@gmail.com>
2023-12-17 09:13:01 -0500

    update: basic refresher on python

--- applied-python-cryptography/refresher.py
+++ applied-python-cryptography/refresher.py
@@ -11,7 +11,25 @@ my_list = [1, "text", 4.5, 5]
 
 my_dictionary = {'name': 'bob', 'age': 53, 'height': 1.83}
 
-print (my_dictionary['name'])
+# print (my_dictionary['name'])
 
-print (my_name[0].isupper())
-print (my_name[0].islower())
+# print (my_name[0].isupper())
+# print (my_name[0].islower())
+
+def add_numbers(number1, number2):
+    return number1 + number2
+
+results = add_numbers(1, 2)
+print (results)
+results = 16
+sqrt_result = sqrt(results)
+print (sqrt_result)
+
+
+start = 1
+end = 10
+for i in range(start, end + 1):
+    if i % 2 == 0:
+        print (i, " is even")
+    else:
+        print (i, " is odd")
